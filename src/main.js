5aa6e3e4927b5d74bfe4866105fadcad85c76562
hamllock <h4mlock@gmail.com>
2023-10-15 21:43:19 +0800

    initial commit

---
+++ src/main.js
@@ -0,0 +1,19 @@
+import "../styles/modern-normalize.css";
+import "../styles/style.css";
+import "../styles/components/header.css";
+import "../styles/components/hero.css";
+import "../styles/utils.css";
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
+    }
+  });
+});
+
+const hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((el) => observer.observe(el));
