d0b5deb71a7694aae6483c500bad350f83890d25
hamllock <h4mlock@gmail.com>
2024-01-31 15:40:32 +0800

    Add scroll-snap-type property to html element and add second.css file

--- src/main.js
+++ src/main.js
@@ -1,20 +1,21 @@
 import "../styles/modern-normalize.css";
 import "../styles/style.css";
 import "../styles/components/first.css";
+import "../styles/components/second.css";
 import "../styles/components/header.css";
 import "../styles/components/hero.css";
 import "../styles/utils.css";
 
-const observer = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    console.log(entry);
-    if (entry.isIntersecting) {
-      entry.target.classList.add("show");
-    } else {
-      entry.target.classList.remove("show");
-    }
-  });
-});
+// const observer = new IntersectionObserver((entries) => {
+//   entries.forEach((entry) => {
+//     console.log(entry);
+//     if (entry.isIntersecting) {
+//       entry.target.classList.add("show");
+//     } else {
+//       entry.target.classList.remove("show");
+//     }
+//   });
+// });
 
-const hiddenElements = document.querySelectorAll(".hidden");
-hiddenElements.forEach((el) => observer.observe(el));
+// const hiddenElements = document.querySelectorAll(".hidden");
+// hiddenElements.forEach((el) => observer.observe(el));
